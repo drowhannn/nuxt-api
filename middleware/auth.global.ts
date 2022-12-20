81d1beb96dda01626b57e1c57ef5d0837c5ef6d8
Rohan Dhimal <drowhann@gmail.com>
2022-12-20 09:57:01 +0545

    feat: modify auth

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,5 +4,7 @@ export default defineNuxtRouteMiddleware((to) => {
   const loginStore = useLoginStore()
   if (!loginStore.isLoggedIn && to.path !== '/' && to.path !== '/register') {
     navigateTo('/')
+  } else if (loginStore.isLoggedIn && (to.path === '/' || to.path === '/register')) {
+    navigateTo('/home')
   }
 })
